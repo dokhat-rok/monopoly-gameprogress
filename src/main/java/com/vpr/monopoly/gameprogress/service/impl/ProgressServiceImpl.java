9b0aa27fc0273228b9c93e7c5d6d413386bfe2ca
KJLVL <uliyakop@gmail.com>
2022-11-09 23:49:36 +0300

    Сделала startGame

--- src/main/java/com/vpr/monopoly/gameprogress/service/impl/ProgressServiceImpl.java
+++ src/main/java/com/vpr/monopoly/gameprogress/service/impl/ProgressServiceImpl.java
@@ -1,14 +1,16 @@
 package com.vpr.monopoly.gameprogress.service.impl;
 
-import com.vpr.monopoly.gameprogress.model.ActionDto;
-import com.vpr.monopoly.gameprogress.model.StartDataDto;
+import com.vpr.monopoly.gameprogress.model.*;
 import com.vpr.monopoly.gameprogress.repository.SessionRepository;
 import com.vpr.monopoly.gameprogress.service.ProgressService;
 import com.vpr.monopoly.gameprogress.service.ServicesManager;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -19,6 +21,9 @@ public class ProgressServiceImpl implements ProgressService {
 
     private final ServicesManager servicesManager;
 
+    @Value("${start.player.money}")
+    private Long money;
+
     @PostConstruct
     private void init(){
         servicesManager.checkConnect();
@@ -26,7 +31,26 @@ public class ProgressServiceImpl implements ProgressService {
 
     @Override
     public StartDataDto startGame(Long count, String[] players) {
-        return null;
+        ArrayList<PlayerDto> newPlayers = new ArrayList<>();
+
+        for (String player: players) {
+            newPlayers.add(PlayerDto.builder()
+                    .playerFigure(player)
+                    .money(money)
+                    .build());
+        }
+
+        sessionRepository.set(
+                "1",
+                SessionDto.builder()
+                        .players(newPlayers)
+                        .build()
+                );
+
+        return StartDataDto.builder()
+                .token(LocalDateTime.now().toString())
+                .players(newPlayers)
+                .build();
     }
 
     @Override
