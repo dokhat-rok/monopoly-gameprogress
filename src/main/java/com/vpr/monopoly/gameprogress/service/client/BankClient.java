e1f9625357c076a8a5e350f53bfff8ca2ab3a4e7
dokhat-rok <dima0409.crivenko@yandex.ru>
2022-11-15 02:38:07 +0300

    Реализовал BankServiceImpl и BankClient, добавил default методы в MonopolyService

--- src/main/java/com/vpr/monopoly/gameprogress/service/client/BankClient.java
+++ src/main/java/com/vpr/monopoly/gameprogress/service/client/BankClient.java
@@ -1,31 +1,71 @@
 package com.vpr.monopoly.gameprogress.service.client;
 
 import com.vpr.monopoly.gameprogress.model.ActionDto;
+import com.vpr.monopoly.gameprogress.model.PlayerDto;
+import com.vpr.monopoly.gameprogress.model.enam.ActionType;
 import com.vpr.monopoly.gameprogress.service.BankService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+import java.util.Map;
 
 @Component
+@Slf4j
 @RequiredArgsConstructor
 public class BankClient implements BankService {
 
-    @Override
-    public ActionDto playerToBankInteraction(ActionDto action) {
-        return null;
+    @Value("${bank.service.base.url}")
+    private String baseUrl;
+
+    private WebClient webClient;
+
+    @PostConstruct
+    private void init(){
+        webClient = WebClient.create(baseUrl);
     }
 
     @Override
-    public boolean isPlayerToBankInteraction(ActionDto action) {
-        return false;
+    public ActionDto playerToBankInteraction(ActionDto action) {
+        String uri = "/tobank";
+        return this.connectByAction(webClient, baseUrl, uri, HttpMethod.PUT, action, log);
+    }
+
+    @Override
+    public Boolean isPlayerToBankInteraction(ActionDto action) {
+        String uri = "/istobank";
+        return this.connectByIsAction(webClient, baseUrl, uri, HttpMethod.PUT, action, log);
     }
 
     @Override
     public ActionDto playerToPlayerInteraction(ActionDto action) {
-        return null;
+        String uri = "/toplayer";
+        return this.connectByAction(webClient, baseUrl, uri, HttpMethod.PUT, action, log);
     }
 
     @Override
-    public boolean isPlayerToPlayerInteraction(ActionDto action) {
-        return false;
+    public Boolean isPlayerToPlayerInteraction(ActionDto action) {
+        String uri = "/istoplayer";
+        return this.connectByIsAction(webClient, baseUrl, uri, HttpMethod.PUT, action, log);
+    }
+
+    @Override
+    public Boolean checkConnection() {
+        List<PlayerDto> playerList = List.of(PlayerDto.builder().build());
+        Long money = 100L;
+        ActionDto action = ActionDto.builder()
+                .actionType(ActionType.MONEY_OPERATION.getLabel())
+                .actionBody(Map.of(
+                        "playerList", playerList,
+                        "money", money
+                ))
+                .build();
+
+        return this.isPlayerToBankInteraction(action) != null;
     }
 }
